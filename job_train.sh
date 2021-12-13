a682955acb7cce3be03d44d28ab22eda631b4af4
vunhatminh <vnmhust@gmail.com>
2021-12-13 14:58:22 -0400

    Update job_train.sh

--- job_train.sh
+++ job_train.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# python capcha_train.py --model resnet34
+
+python capcha_train.py --model resnet18
+python capcha_train.py --model resnet34
 python capcha_train.py --model resnet50
 python capcha_train.py --model wide_resnet
